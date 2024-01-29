0b3931da569be70581715fc26ac467571a4c2e42
nuralimajid <nuralimajids@gmail.com>
2024-01-29 23:45:17 +0700

    Email Setting

--- McDermott/McDermott.Domain/Entities/EmailSetting.cs
+++ McDermott/McDermott.Domain/Entities/EmailSetting.cs
@@ -3,22 +3,16 @@
     public partial class EmailSetting : BaseAuditableEntity
     {
         [StringLength(300)]
-        public string Name { get; set; }
-        public int Sequence { get; set; }
-        public bool Smpt_Debug { get; set; }
-        public Encripts Smtp_Encryption { get; set; }
+        public string? Description { get; set; }
+        public int? Sequence { get; set; }
+        public bool? Smpt_Debug { get; set; }
+        public string Smtp_Encryption { get; set; }
         [StringLength(200)]
-        public string Smtp_Host { get; set; }
-        public string Smtp_Pass { get; set; }
-        public int Smtp_Port { get; set; }
+        public string? Smtp_Host { get; set; }
+        public string? Smtp_Pass { get; set; }
+        public int? Smtp_Port { get; set; }
         [StringLength(200)]
-        public string Smtp_User { get; set; }
+        public string? Smtp_User { get; set; }
 
-
-
-        public enum Encripts
-        {
-            none, TLS, SSL
-        }
     }
 }
