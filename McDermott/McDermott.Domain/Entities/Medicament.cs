9d44f6ae5b9c21052c51b4909dc0dd0cd6fa3b9c
Ali ih <nuralimajids@gmail.com>
2024-04-01 16:24:04 +0700

    Medicament part 1

---
+++ McDermott/McDermott.Domain/Entities/Medicament.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McDermott.Domain.Entities
+{
+    public class Medicament :BaseAuditableEntity
+    {
+        public string? Name { get; set; }
+        public int? PhycisianId { get; set; }
+    }
+}
---
+++ McDermott/McDermott.Web/Components/Pages/Pharmacy/MedicamentPage.razor.cs
@@ -0,0 +1,74 @@
+﻿using McDermott.Domain.Entities;
+using MediatR;
+using static McDermott.Application.Features.Commands.Pharmacy.SignaCommand;
+
+namespace McDermott.Web.Components.Pages.Pharmacy
+{
+    public partial class MedicamentPage
+    {
+        #region Relation Data
+        private List<MedicamentDto> medicaments = [];
+        #endregion
+        #region variabel static
+        private bool PanelVisible { get; set; } = false;
+        private IReadOnlyList<object> SelectedDataItems { get; set; } = [];
+        #endregion
+
+        #region UserLoginAndAccessRole
+
+        [Inject]
+        public UserInfoService UserInfoService { get; set; }
+
+        private GroupMenuDto UserAccessCRUID = new();
+        private User UserLogin { get; set; } = new();
+        private bool IsAccess = false;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                try
+                {
+                    await GetUserInfo();
+                }
+                catch { }
+            }
+        }
+
+        private async Task GetUserInfo()
+        {
+            try
+            {
+                var user = await UserInfoService.GetUserInfo();
+                IsAccess = user.Item1;
+                UserAccessCRUID = user.Item2;
+                UserLogin = user.Item3;
+            }
+            catch { }
+        }
+
+        #endregion UserLoginAndAccessRole
+
+        #region async Data
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                await GetUserInfo();
+            }
+            catch { }
+
+            await LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            PanelVisible = true;
+            SelectedDataItems = new ObservableRangeCollection<object>();
+            PanelVisible = false;
+        }
+        #endregion
+    }
+}
