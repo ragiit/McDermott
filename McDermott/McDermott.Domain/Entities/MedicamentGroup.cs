65fd00cba07f3b05df13b11f1927a4df6783304d
Ali ih <nuralimajids@gmail.com>
2024-04-01 17:50:03 +0700

    Medicament Group

---
+++ McDermott/McDermott.Domain/Entities/MedicamentGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McDermott.Domain.Entities
+{
+    public class MedicamentGroup :BaseAuditableEntity
+    {
+        public string? Name { get; set; }
+        public bool? IsConcoction { get; set; }
+        public int? PhycisianId { get; set; }
+        public int? MedicaneName { get; set; }
+        public int? ActiveComponent { get; set; }
+        public int? RegimentOfUseId { get; set; }
+        public float? MedicaneUnitDosage { get; set; }
+        public float? QtyByDay { get; set; }
+        public float? Days { get; set; }
+        public float? TotalQty { get; set; }
+        public string? UnitOfDosage { get; set; } 
+        public string? Form { get; set; }
+        public string? UnitOfDosageCategory { get; set; }
+        public string? Comment { get; set; }
+    }
+}
