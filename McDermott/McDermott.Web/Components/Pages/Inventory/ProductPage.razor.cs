b482c311a0fe50c3fb7f5a1a7d91f4209bfd451c
Ali ih <nuralimajids@gmail.com>
2024-04-25 13:22:16 +0700

    no message

--- McDermott/McDermott.Web/Components/Pages/Inventory/ProductPage.razor.cs
+++ McDermott/McDermott.Web/Components/Pages/Inventory/ProductPage.razor.cs
@@ -185,7 +185,7 @@ namespace McDermott.Web.Components.Pages.Inventory
                     Name = group.FirstOrDefault()?.Name, // Use FirstOrDefault() for safer handling
                     SalesPrice = group.FirstOrDefault()?.SalesPrice,
                     InternalReference = group.FirstOrDefault()?.InternalReference,
-                    Qtys = group.Where(product => product.StockProducts.StatusTransaction == "IN").Sum(product => product.StockProducts?.Qty) - group.Where(product => product.StockProducts?.StatusTransaction == "OUT").Sum(product => product.StockProducts?.Qty),
+                    Qtys = group.Where(product => product.StockProducts?.StatusTransaction == "IN").Sum(product => product.StockProducts?.Qty) ,
                     UomName = group.FirstOrDefault()?.Uom.Name
                 }).ToList();
 
