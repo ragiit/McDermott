dc265a2ed995cdf5b23beed704b855c6d66c5c62
Ali ih <nuralimajids@gmail.com>
2024-05-14 18:31:07 +0700

    Penambahan Relation Medicament Group

--- McDermott/McDermott.Application/Dtos/Pharmacy/PrescriptionDto.cs
+++ McDermott/McDermott.Application/Dtos/Pharmacy/PrescriptionDto.cs
@@ -5,17 +5,22 @@
         public long Id { get; set; }
         public long PharmacyId { get; set; }
         public long? DrugFromId { get; set; }
-        public long? SignaId { get; set; }
         public long? DrugRouteId { get; set; }
         public long? DrugDosageId { get; set; }
+        public long? MedicamentGroupId { get; set; }
         public long? ProductId { get; set; }
-        public float Stock { get; set; } // product stock
-        public float PrescribedAmount { get; set; } // jumlah yg diresepkan
-        public float GivenAmount { get; set; } // jumlah yg diberikan
-        public float PriceUnit { get; set; }
-        public float PreTaxPrescription { get; set; }
-        public float PreTaxConcoction { get; set; }
-        public float Total { get; set; }
+        public long? UomId { get; set; }
+        public string? ProductName { get; set; }
+        public string? DosageFrequency { get; set; }
+        public string? DrugRoutName { get; set; }
+        public string? DrugDosageName { get; set; }
+        public long? Stock { get; set; } // product stock
+        public long? PrescribedAmount { get; set; } // jumlah yg diresepkan
+        public long? GivenAmount { get; set; } // jumlah yg diberikan
+        public long? PriceUnit { get; set; }
+        public long? PreTaxPrescription { get; set; }
+        public long? PreTaxConcoction { get; set; }
+        public long? Total { get; set; }
 
         public DrugFormDto? DrugForm { get; set; }
         public PharmacyDto? Pharmacy { get; set; }
@@ -23,5 +28,6 @@
         public SignaDto? Signa { get; set; }
         public DrugRouteDto? DrugRoute { get; set; }
         public DrugDosageDto? DrugDosage { get; set; }
+        public MedicamentGroupDto? MedicamentGroup { get; set; }
     }
 }
