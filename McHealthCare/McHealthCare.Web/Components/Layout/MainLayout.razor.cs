cd9df263b5eeba270b5e5a401cb78e68df05b827
Argi P <ragiithub@gmail.com>
2024-06-13 11:22:41 +0700

    no message

--- McHealthCare/McHealthCare.Web/Components/Layout/MainLayout.razor.cs
+++ McHealthCare/McHealthCare.Web/Components/Layout/MainLayout.razor.cs
@@ -1,8 +1,38 @@
-﻿namespace McHealthCare.Web.Components.Layout
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace McHealthCare.Web.Components.Layout
 {
     public partial class MainLayout
     {
         private bool showPreloader = true;
+        private string currentNav;
+
+        protected override void OnInitialized()
+        {
+            currentNav = NavigationManager.Uri;
+            NavigationManager.LocationChanged += OnLocationChanged;
+        }
+
+        private void OnLocationChanged(object sender, LocationChangedEventArgs e)
+        {
+            currentNav = e.Location;
+            InvokeAsync(StateHasChanged);
+        }
+
+        private string IsActiveNav(string navItem)
+        {
+            if (navItem.Contains("home"))
+                return "active";
+
+            var a = currentNav.Contains(navItem) ? "active" : "";
+
+            return a;
+        }
+
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= OnLocationChanged;
+        }
 
         protected override async Task OnInitializedAsync()
         {
--- McHealthCare/McHealthCare.Web/Data/Migrations/20240613041858_A.Designer.cs
+++ McHealthCare/McHealthCare.Web/Data/Migrations/20240613041858_A.Designer.cs
@@ -4,6 +4,7 @@ using McHealthCare.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace McHealthCare.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240613041858_A")]
+    partial class A
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
