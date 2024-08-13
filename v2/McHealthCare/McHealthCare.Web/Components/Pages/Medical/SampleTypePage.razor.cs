0aec6115fa1abbd6867c78467795959810debb9a
Ali Ih <nuralimajids@gmail.com>
2024-08-13 17:04:23 +0700

    no message

---
+++ v2/McHealthCare/McHealthCare.Web/Components/Pages/Medical/SampleTypePage.razor.cs
@@ -0,0 +1,34 @@
+﻿using McHealthCare.Application.Dtos.Medical;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace McHealthCare.Web.Components.Pages.Medical
+{
+    public partial class SampleTypePage
+    {
+        #region Relation Data
+        private List<SampleTypeDto> getSampleType = [];
+        private SampleTypeDto postSampleType = new();
+        #endregion
+        #region Variabel
+        private bool PanelVisible { get; set; } = false;
+        private (bool, GroupMenuDto) UserAccess { get; set; } = new();
+        private bool IsLoading { get; set; } = true;
+        private HubConnection? hubConnection;
+
+        private List<ExportFileData> ExportFileDatas =
+        [
+            new()
+            {
+                Column = "Name",
+                Notes = "Mandatory"
+            },
+
+        ];
+
+        private int FocusedRowVisibleIndex { get; set; }
+        public IGrid Grid { get; set; }
+        private IReadOnlyList<object> SelectedDataItems { get; set; } = [];
+        #endregion
+
+    }
+}
