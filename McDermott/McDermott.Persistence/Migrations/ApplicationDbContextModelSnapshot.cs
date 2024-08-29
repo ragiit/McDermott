e91b497f1f54465058bd1e76ff913aeefc17fb31
Argi <ragiithub@gmail.com>
2024-08-29 16:04:47 +0700

    Conflict

--- McDermott/McDermott.Persistence/Migrations/ApplicationDbContextModelSnapshot.cs
+++ McDermott/McDermott.Persistence/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4769,6 +4769,9 @@ namespace McDermott.Persistence.Migrations
                     b.Property<bool>("IsPatient")
                         .HasColumnType("bit");
 
+                    b.Property<bool>("IsTelemedicine")
+                        .HasColumnType("bit");
+
                     b.Property<bool>("IsVaccination")
                         .HasColumnType("bit");
 
